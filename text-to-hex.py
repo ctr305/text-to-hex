2d839869aebbc0ed3014fc21f224ac9714264996
Leonardo Criollo <criollo_banneret@tutanota.com>
2023-08-21 16:51:03 -0600

    Implement Hex/Binary Functionality.

--- text-to-hex.py
+++ text-to-hex.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+import sys
+
+# Read command line argument as string
+text = sys.argv[1]
+
+# Read each character in the string
+print("Text in binary: ", text)
+for char in text:
+    # Print the character as binary
+    print(bin(ord(char)), end=' ')
+
+# Print a new line before printing the hex
+print("\nText in hex: ", text)
+for char in text:
+    # Print the character as hex
+    print(hex(ord(char)), end=' ')
+
+print()
